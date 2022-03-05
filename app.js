f6154f9ac5fc054fcca31e6adc9857ba7bd3fabc
AbhilashS05 <88608929+AbhilashS05@users.noreply.github.com>
2022-03-05 21:54:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://abhi:test123@cluster0.vjhma.mongodb.net/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then((result) => app.listen(PORT))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/learning', (req, res) => res.render('learning'));
+app.get('/profile', requireAuth, (req, res) => res.render('profile'));
+
+app.use(authRoutes);
